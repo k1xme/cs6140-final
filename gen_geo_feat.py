28090360502c3ae307fce40a3b804b859d17f44b
Kexi <longkexi@gmail.com>
2016-12-04 20:35:13 -0500

    fixed parent category looking up

--- gen_geo_feat.py
+++ gen_geo_feat.py
@@ -1,17 +1,33 @@
 import pymongo
-import pandas
+import json
+import collections
 
 yelp_db = pymongo.MongoClient().yelp_data
 businesses = yelp_db.businesses
 geo_feats = yelp_db.geo_feats
 radius = 0.5/3963.2 # walking distance.
 min_rating = 3.5
+parent_categories = collections.defaultdict(set)
 
-def count_category(nearby_biz, category):
+def get_parent_categories():
+    with open('./categories.json') as all_category_file:
+        for i, category in enumerate(json.loads(all_category_file.read())):
+            if 'country_whitelist' in category and \
+               'US' not in category['country_whitelist']:
+                continue
+
+            if 'country_blacklist' in category and \
+               'US' in category['country_blacklist']:
+                continue
+
+            parent_categories[category['title']] |= set(category['parents'])
+            parent_categories[category['alias']] |= set(category['parents'])
+
+def count_category(nearby_biz, target):
     count = 0
     for biz in nearby_biz:
-        if category in biz['categories']:
-            count += 1
+        for c in biz['categories']:
+            if c in parent_categories and target in parent_categories[c]: count += 1
     return count
 
 def count_high_rating_biz(nearby_biz):
@@ -22,6 +38,7 @@ def count_high_rating_biz(nearby_biz):
     return count
 
 def main():
+    get_parent_categories()
     count = 0
     geo_feat_docs = []
     for biz in businesses.find():
